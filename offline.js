dc52460d08643b3a45f2cce64ddd0598b9685573
Tombas Tomáš Basovník <tombasonly@gmail.com>
2018-02-26 17:08:22 +0100

    v2

---
+++ offline.js
@@ -0,0 +1,1 @@
+{"version":1519661273975,"fileList":["gamee-js.min.js","c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/ef_line-sheet0.png","images/shared-0-sheet0.png","opus.js","opus.asm.js","opus.asm.js.mem","opus.wasm.js","opus.wasm.wasm","media/2_japodeziloop.webm","icons/icon-16.png","icons/icon-32.png","icons/loading-logo.png","register-sw.js"]}
